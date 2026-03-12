ea3cb0dc098ce0cb703f0a5102248c52317c42e4
letscoderitik <letscoderitik@gmail.com>
2026-03-12 11:58:15 +0530

    Update declare_Node.py

--- declare_Node.py
+++ declare_Node.py
@@ -1,5 +1,10 @@
 class Node:
-  def __init__ (self, data):
-    self.data = data
-    self.next = None
+    def __init__ (self, data):
+        self.data = data
+        self.next = None
+        
+def createNode(data):
+    newNode= Node(data);
+    print(" -Node at ", newNode, "\n -Data- ", newNode.data )
 
+createNode(10);
